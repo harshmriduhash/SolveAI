2682ab80b6dc700050c52b8b9075d30edc3122ba
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2024-04-07 22:41:09 +0530

    New

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -10,7 +10,6 @@ export default function Home() {
 				<div className="relative isolate overflow-hidden pt-14">
 					<img src={background1} alt=" ai" className="absolute inset-0 -z-10 h-full w-full object-cover" />
 					<div className="absolute inset-0 -z-10 bg-opacity-90"></div>
-
 					<div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
 						<div className="hidden sm:mb-8 sm:flex sm:justify-center">
 							<div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-400 ring-1 ring-white/10 hover:ring-white/20">
