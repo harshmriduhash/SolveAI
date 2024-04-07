e2dd77cf5784ba15e30525762f3972c3f2946224
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2024-04-07 22:39:54 +0530

    New

--- frontend/src/components/Home/FreeTrial.js
+++ frontend/src/components/Home/FreeTrial.js
@@ -8,7 +8,7 @@ export default function FreeTrial() {
 					<h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
 						Boost your productivity.
 						<br />
-						Start using our app today.
+						Start using our app from today.
 					</h2>
 					<p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
 						Boost your productivity with SolveAI Generator. Start using our app today.
@@ -17,7 +17,7 @@ export default function FreeTrial() {
 						<Link
 							to="/register"
 							className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
-							Start 3 Day Free Trial
+							Start your 3 Day Free Trial
 						</Link>
 						<Link to="features" className="text-sm font-semibold leading-6 text-white">
 							Learn more <span aria-hidden="true">→</span>
