092088567057c6471ee98ea2de11da9070462daf
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2024-04-08 22:40:50 +0530

    New

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -1,10 +1,10 @@
 const errorHandler = (error, req, res, next) => {
-	const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-	res.status(statusCode);
-	res.json({
-		message: error.message,
-		stack: process.env.NODE_ENV === 'production' ? error.stack : {},
-	});
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === "production" ? error.stack : {},
+  });
 };
 
 module.exports = { errorHandler };
